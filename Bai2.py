852862b9c78610e4f08cd1e65db5cab06e49a559
DaoHuy <huy.edi.1210@gmail.com>
2026-03-28 19:14:49 +0700

    Restore Streamlit data display functionality

--- Bai2.py
+++ Bai2.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-data=pd.read_csv("Bai2-1.csv")
-#st.write(data)
-#st.table(data)
+data = pd.read_csv("Bai2-1.csv")
+
+st.write(data)
+st.table(data)
+
 st.dataframe(data, width=None, height=None)
