e9b0433333261951f05d5dee1ff8da7b1c130ddd
DaoHuy <huy.edi.1210@gmail.com>
2026-03-28 19:28:10 +0700

    Change CSV source and DataFrame index in Bai2.py
    Updated CSV file and DataFrame index for clarity.

--- Bai2.py
+++ Bai2.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv("Bai2-1.csv")
-
+data = pd.read_csv("data_lesson2_none_header.csv")
+df = pd.DataFrame(data, index=["Thời gian", "Nội dung", "Địa điểm", "Cảm xúc"])
 st.write(data)
 st.table(data)
-
 st.dataframe(data, width=None, height=None)
