798276b482e3ed8f56db3fbcad635320c49ade8a
DaoHuy <huy.edi.1210@gmail.com>
2026-03-28 19:02:27 +0700

    Change CSV file source from Bai2.csv to Bai2-1.csv

--- Bai2.py
+++ Bai2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-data=pd.read_csv("Bai2.csv")
+data=pd.read_csv("Bai2-1.csv")
 #st.write(data)
 #st.table(data)
 st.dataframe(data, width=None, height=None)
